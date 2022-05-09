28dd49bcee59ecd74e566804af7be6bd05b4bd6f
DONKER963 <cuentss84@gmail.com>
2022-05-09 15:22:55 -0500

    update

--- pedidos/ped.py
+++ pedidos/ped.py
@@ -1,1 +1,10 @@
+import tkinter as tk
+#from client.gui_app import *
 
+def main():
+    root = tk.Tk() #raiz
+    root.title('Loggin Cliente')
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
