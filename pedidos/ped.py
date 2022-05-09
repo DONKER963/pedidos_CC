98aea1ec019ff7e2461a2bbc70e8e019dd98d10f
Marcos Campos <105240161+ImSoAznn12@users.noreply.github.com>
2022-05-09 14:58:03 -0500

    Update ped.py

--- pedidos/ped.py
+++ pedidos/ped.py
@@ -4,4 +4,10 @@ import pymysql
 def tkinter import messagebox
 def primeraconexion();
   try:
-        miconexion = pymysql.connect(host="localhost", user="root".)
+        miconexion = pymysql.connect(host="localhost", user="root", password="", db="mysql")
+        micursor = miconexion.cursor()
+        micursor.execute("CREATE DATABASE IF NOT EXIST empresa")
+        miconexion.close()
+        micursor.close()
+        #creacion de la tabla
+        
